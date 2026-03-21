6124edabf39deee8d59127ea2ed5ba05a6b0fa8e
franckmonmardy-cell <franckmonmardy@gmail.com>
2026-03-21 19:11:21 +0000

    Create status.js

---
+++ api/status.js
@@ -0,0 +1,22 @@
+const API_KEY = process.env.API_FOOTBALL_KEY || 'e2ec443674851ecb52aef5605084fe61';
+const API_BASE = 'https://v3.football.api-sports.io';
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  try {
+    const response = await fetch(`${API_BASE}/status`, {
+      headers: {
+        'x-rapidapi-key': API_KEY,
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+      }
+    });
+    const data = await response.json();
+    res.setHeader('Cache-Control', 's-maxage=60');
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message, response: [] });
+  }
+};
